a122ca44291275b2cf4e493e25c6a4fd3d63a8e6
GuilhermeUberti <guilherme.uberti@hotmail.com>
2022-07-10 16:49:16 -0300

    Create product, enviando imagem e armazenando no backend

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,23 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+import { ListCategoryController } from './controllers/category/ListCategoryController';
+
+import { CreateProductController } from './controllers/product/CreateProductController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
+import uploadConfig from './config/multer';
+
 const router = Router();
+
+// folder de armazenamento de imagens
+const upload = multer(uploadConfig.upload("./tmp"));
 
 //Rotas user
 router.post('/users', new CreateUserController().handle); //create user
@@ -17,6 +26,9 @@ router.get('/detail', isAuthenticated, new DetailUserController().handle); //det
 
 //Rotas Category
 router.post('/category', isAuthenticated, new CreateCategoryController().handle);
+router.get('/categories', isAuthenticated, new ListCategoryController().handle);
 
+//Rotas Product
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle);
 
 export { router };
